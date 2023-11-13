00029f98017ae92a55952c6ada6dd3c45096262c
fasilofficial <fasilofficialofficial@gmail.com>
2023-11-13 13:31:27 +0530

    Bug fixed

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-import TELEGRAM_BOT_TOKEN from "./secrets.js";
-import { Telegraf } from "telegraf";
+require('dotenv').config()
+const {Telegraf} = require('telegraf')
 
 const randomId = () => {
-  // Generate a random ID between 1 and 1000
   return Math.round(Math.random() * (1001 - 0) + 0);
 };
 
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.command("generate", async (ctx) => {
   let imageUrl = `https://picsum.photos/id/${randomId()}/1920/1080`;
-  ctx.replyWithPhoto({ url: imageUrl });
+  if(imageUrl) {
+    return ctx.replyWithPhoto({ url: imageUrl });
+  }
+  imageUrl = `https://picsum.photos/id/${randomId()}/1920/1080`;
+  
 });
 
 bot.launch();
